06e2326d33b6fb5323af00315907b1bcc8aeccf2
MagnusVenatus <nico_bv@live.com>
2025-11-15 14:28:43 -0300

    Add files via upload
    Actividad 2 Web

---
+++ ActNube2/app/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 6969;
+
+app.use(express.static(path.join(__dirname)));
+
+// Ruta principal
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Iniciar el servidor
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
